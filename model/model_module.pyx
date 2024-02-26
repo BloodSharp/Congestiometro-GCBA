069a7c3f6fc5a1b5f38e457c3155f92b09f22f69
Agustín Alejandro dos Santos <a.dossantos@buenosaires.gob.ar>
2024-02-26 14:43:44 -0300

    Actualizar contenedor 'model', modificando por completo la configuración del contenedor y conviertiendo la aplicación web de Flask como módulo nativo. A su vez se genera un programa también nativo, el cual carga el modelo previamente entrenado y su servidor http. Este cambio se hizo de forma que la aplicación será mucho más eficiente que antes al no tener que utilizar el interprete de python.

---
+++ model/model_module.pyx
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import psycopg2
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+try:
+    model = tf.keras.models.load_model("./model")
+except:
+    from train import train
+
+    model = train()
+
+postgres_conn = psycopg2.connect(
+    **{
+        "host": "postgres",
+        "port": 5432,
+        "user": "postgres",
+        "password": "congestiometro",
+        "dbname": "postgres",
+    }
+)
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        params = request.json["params"]
+        with open("./query.sql") as query_string:
+            q = query_string.read()
+            with postgres_conn.cursor() as cursor:
+                cursor.execute(q, params)
+                res = cursor.fetchall()
+                return model(res)
+    except Exception as err:
+        app.logger.error(err)
+        return jsonify({"status": "unexpected error"}), 500
+
+cdef public run_prediction_model_main():
+    app.run(host="0.0.0.0", port=6001, debug=True)
---
+++ model/setup.py
@@ -0,0 +1,53 @@
+import os
+import sysconfig
+from setuptools import find_packages, setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+# https://stackoverflow.com/questions/38523941/change-cythons-naming-rules-for-so-files
+def get_ext_filename_without_platform_suffix(filename, suffix):
+    name, ext = os.path.splitext(filename)
+    #name, ext = os.path.split(filename, '.')
+    ext_suffix = suffix
+
+    if ext_suffix == ext:
+        return filename
+
+    ext_suffix = ext_suffix.replace(ext, '')
+
+    idx = name.find(ext_suffix)
+
+    if idx == -1:
+        return filename
+    else:
+        return name[:idx] + ext
+
+class BuildExtWithoutPlatformSuffix(build_ext):
+    # def get_ext_fullpath(self, ext_name):
+    #     filename = super().get_ext_filename(ext_name)
+    #     modified_filename = get_ext_filename_without_platform_suffix(filename, sysconfig.get_config_var('EXT_SUFFIX'))
+    #     modified_filename = get_ext_filename_without_platform_suffix(modified_filename, sysconfig.get_config_var('py_version_short'))
+    #     return get_ext_filename_without_platform_suffix(modified_filename, sys.platform)
+        #return get_ext_filename_without_platform_suffix(modified_filename, sys.platform + sysconfig.get_config_var('py_version_short'))
+    def get_ext_filename(self, ext_name):
+        filename = super().get_ext_filename(ext_name)
+        modified_filename = get_ext_filename_without_platform_suffix(filename, sysconfig.get_config_var('EXT_SUFFIX'))
+        return 'lib'+modified_filename
+        #modified_filename = get_ext_filename_without_platform_suffix(modified_filename, sysconfig.get_config_var('py_version_short'))
+        #return get_ext_filename_without_platform_suffix(modified_filename, sys.platform + sysconfig.get_config_var('py_version_short'))
+
+setup(
+    cmdclass={'build_ext': BuildExtWithoutPlatformSuffix},
+    name="modelo_ia",
+    version="3",
+    ext_modules=cythonize(
+        [
+            Extension(
+                name="model_module",
+                sources=["model_module.pyx"],
+            ),
+        ],
+        build_dir="build",
+    ),
+)
